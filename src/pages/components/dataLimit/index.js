a9f7e36908e63c89a33f993b5ad8bfff389c34b1
ZCode75 <amir.sr75@yahoo.com>
2023-10-11 13:51:11 +0330

    add: duration and data limit cmp

---
+++ src/pages/components/dataLimit/index.js
@@ -0,0 +1,56 @@
+import { Grid } from '@mui/material';
+import Slider from 'components/formik/slider';
+import TextField from 'components/formik/textfield';
+import { styled } from '@mui/material/styles';
+
+const Input = styled(TextField)`
+  width: 75px;
+`;
+
+function valueLabelFormat(value) {
+  const unit = 'GB';
+  let scaledValue = value;
+
+  return `${scaledValue} ${unit}`;
+}
+
+function calculateValue(value) {
+  return value;
+}
+
+const DataLimit = ({ disabled }) => {
+  return (
+    <Grid container spacing={2} alignItems="center">
+      <Grid item>
+        <Input
+          disabled={disabled}
+          fullWidth={false}
+          size="small"
+          name={'data_limit'}
+          inputProps={{
+            step: 1,
+            min: 0,
+            max: 500,
+            type: 'number',
+            'aria-labelledby': 'input-slider'
+          }}
+        />
+      </Grid>
+      <Grid item xs>
+        <Slider
+          disabled={disabled}
+          name={'data_limit'}
+          min={0}
+          max={500}
+          step={1}
+          scale={calculateValue}
+          getAriaValueText={valueLabelFormat}
+          valueLabelFormat={valueLabelFormat}
+          valueLabelDisplay="auto"
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default DataLimit;
