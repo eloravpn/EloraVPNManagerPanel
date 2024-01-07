5ecf5caf886d6574c66cd5ccad0a15537c0a7a3f
ZCode75 <amir.sr75@yahoo.com>
2024-01-07 10:02:36 +0330

    update pkgs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Routes from './routes';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import reducers from './store/reducers';
 import { memo } from 'react';
+import store from './store';
 
 function App() {
   return (
-    <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+    <Provider store={store}>
       <Routes />
     </Provider>
   );
