0c6144672f34f36b0f009f95c21aed9d6c374764
ZCode75 <amir.sr75@yahoo.com>
2024-04-12 18:33:41 +0330

    add notifications bulks send

---
+++ src/pages/notifications/AddOne.js
@@ -0,0 +1,48 @@
+import { Grid } from '@mui/material';
+import CheckBox from 'components/formik/checkbox';
+import Select from 'components/formik/select';
+import TextField from 'components/formik/textfield';
+import GLOBAL from 'components/variables';
+
+const AddOneFrom = () => {
+  return (
+    <>
+      <Grid item xs={12} md={3}>
+        <Select id={'status'} name={'status'} label="Status" options={GLOBAL.statusNotifications} />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Select id={'type'} name={'type'} label="Type" options={GLOBAL.typeNotifications} />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Select
+          id={'engine'}
+          name={'engine'}
+          label="Engine"
+          options={[
+            { id: 'telegram', name: 'Telegram' },
+            { id: 'email', name: 'Email' },
+            { id: 'sms', name: 'SMS' }
+          ]}
+        />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <TextField id={'level'} name={'level'} label="Level" />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          id={'message'}
+          name={'message'}
+          label="Message"
+          type="text"
+          minRows={4}
+          multiline
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <CheckBox name="approve" label={'Approve'} />
+      </Grid>
+    </>
+  );
+};
+
+export default AddOneFrom;
