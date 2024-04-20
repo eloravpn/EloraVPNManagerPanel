9641187549cac46b8223c5bac8fd9c32352c5623
ZCode75 <amir.sr75@yahoo.com>
2024-04-20 15:47:38 +0330

    add pring png in usage ACC

--- src/pages/accounts/usages/Usages.js
+++ src/pages/accounts/usages/Usages.js
@@ -1,11 +1,11 @@
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, List, ListItem, Skeleton, Typography } from '@mui/material';
 import Mixed from 'components/chart/Mixed';
 import SelectBadge from 'components/formik/badge';
 import FormObserver from 'components/formik/observer';
 import dayjs from 'dayjs';
 import { Form, Formik } from 'formik';
-import useAccount from 'hooks/useAccount';
-import { memo, useState } from 'react';
+import { toPng } from 'html-to-image';
+import { memo, useRef, useState } from 'react';
 import { getReportAccount } from 'services/reportService';
 import { convertByteToInt, getBetweenDate, getDayPersian } from 'utils';
 
@@ -16,7 +16,7 @@ dayjs.extend(timezone);
 
 const Usages = (props) => {
   const { initial } = props;
-  const { getUsageAccount, useages, isLoading } = useAccount();
+
   const [isLoadingGetReport, setIsLoadingGetReport] = useState(false);
   const [reportHosts, setReportHosts] = useState([]);
   const [labelReportHost, setLabelReportHost] = useState([]);
@@ -81,72 +81,121 @@ const Usages = (props) => {
     }
   };
 
-  return (
-    <Grid container spacing={2}>
-      <Grid
-        item
-        xs={12}
-        md={12}
-        sxContent={{
-          overflowY: 'scroll'
-        }}
-      >
-        <Formik initialValues={{ date: 1 }}>
-          {() => (
-            <Form>
-              <FormObserver onChange={handleSubmit} />
-              <Box
-                mx={0.5}
-                display={'flex'}
-                justifyContent={'center'}
-                alignItems={'center'}
-                bgcolor={'#f6f6f6'}
-                borderRadius={50}
-                py={'auto'}
-              >
-                <SelectBadge
-                  mx={0.9}
-                  px={0.9}
-                  py={0.5}
-                  width={'auto'}
-                  borderRadius={50}
-                  options={[
-                    { name: '24 H', id: 24 },
-                    { name: '1 Day', id: 1 },
-                    { name: '1 Week', id: 7 },
-                    { name: '1 Month', id: 30 }
-                  ]}
-                  name="date"
-                />
-              </Box>
-            </Form>
-          )}
-        </Formik>
+  const htmlToImageConvert = () => {
+    alert();
+    toPng(imageRef.current, {})
+      .then((dataUrl) => {
+        const link = document.createElement('a');
+        link.download = 'Usages.png';
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-        <Mixed
-          type={'area'}
-          data={{
-            labels: labelReportHost,
-            data: [
-              {
-                type: 'column',
-                data: reportHosts.map(({ download }) => convertByteToInt(download).toFixed(2)),
-                name: 'Download'
-              },
-              {
-                type: 'column',
-                data: reportHosts.map(({ upload }) => convertByteToInt(upload).toFixed(2)),
-                name: 'Upload'
-              }
-            ]
-          }}
-          max={Math.max(
-            ...reportHosts.map(({ download }) => convertByteToInt(download).toFixed(2))
-          )}
-          height={350}
-        />
+  const imageRef = useRef();
+
+  return (
+    <>
+      <button onClick={htmlToImageConvert}>Download</button>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={12}>
+          <Formik initialValues={{ date: 1 }}>
+            {() => (
+              <Form>
+                <FormObserver onChange={handleSubmit} />
+                <Box
+                  mx={0.5}
+                  display={'flex'}
+                  justifyContent={'center'}
+                  alignItems={'center'}
+                  bgcolor={'#f6f6f6'}
+                  borderRadius={50}
+                  py={'auto'}
+                >
+                  <SelectBadge
+                    mx={0.9}
+                    px={0.9}
+                    py={0.5}
+                    width={'auto'}
+                    borderRadius={50}
+                    options={[
+                      { name: '24 H', id: 24 },
+                      { name: '1 Day', id: 1 },
+                      { name: '1 Week', id: 7 },
+                      { name: '1 Month', id: 30 }
+                    ]}
+                    name="date"
+                  />
+                </Box>
+              </Form>
+            )}
+          </Formik>
+          <Box textAlign={'center'}>
+            <Typography variant="h6">Total Usage</Typography>
+            <Typography>
+              Download:{' '}
+              {isLoadingGetReport ? (
+                <Skeleton width={35} height={15} sx={{ ml: 1 }} />
+              ) : (
+                convertByteToInt(
+                  reportHosts.reduce((init, curr) => init + curr.download, 0)
+                ).toFixed(2) + 'GB'
+              )}
+            </Typography>
+            <Typography>
+              Upload:{' '}
+              {isLoadingGetReport ? (
+                <Skeleton width={35} height={15} sx={{ ml: 1 }} />
+              ) : (
+                convertByteToInt(reportHosts.reduce((init, curr) => init + curr.upload, 0)).toFixed(
+                  2
+                ) + 'GB'
+              )}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid xs={12}></Grid>
       </Grid>
-    </Grid>
+      <table className="table-print" ref={imageRef}>
+        <tr>
+          <th>Download</th>
+          <th>Upload</th>
+          <th>Date</th>
+        </tr>
+
+        {reportHosts.map(({ download, upload, date }) => (
+          <tr>
+            <td>{convertByteToInt(download).toFixed(2)}</td>
+            <td> {convertByteToInt(upload).toFixed(2)}</td>
+            <td>{getDayPersian(date)}</td>
+          </tr>
+        ))}
+      </table>
+      {/* <Mixed
+        isLoading={isLoadingGetReport}
+        type={'area'}
+        data={{
+          labels: labelReportHost,
+          data: [
+            {
+              type: 'column',
+              data: reportHosts.map(({ download }) => convertByteToInt(download).toFixed(2)),
+              name: 'Download'
+            },
+            {
+              type: 'column',
+              data: reportHosts.map(({ upload }) => convertByteToInt(upload).toFixed(2)),
+              name: 'Upload'
+            }
+          ]
+        }}
+        max={Math.max(...reportHosts.map(({ download }) => convertByteToInt(download).toFixed(2)))}
+        height={350}
+      /> */}
+    </>
   );
 };
 
