e13a0532763d194bd5f5cc5833573ab6a0f01c8a
ZCode75 <amir.sr75@yahoo.com>
2024-06-04 12:25:57 +0330

    add cols in services grid

--- src/pages/services/columns.js
+++ src/pages/services/columns.js
@@ -5,6 +5,22 @@ const columns = [
     width: 200
   },
   {
+    headerName: 'Duration',
+    field: 'duration',
+    width: 100
+  },
+
+  {
+    headerName: 'Price',
+    field: 'price',
+    renderCell: 'price'
+  },
+  {
+    headerName: 'Data(GB)',
+    field: 'data_limit',
+    renderCell: 'datalimit'
+  },
+  {
     headerName: 'Enable',
     field: 'enable',
     renderCell: 'status',
