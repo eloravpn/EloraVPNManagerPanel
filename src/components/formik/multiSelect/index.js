25f84f1262f0b7c427b0a82287a0d1fc4b2e4dc9
ZCode75 <amir.sr75@yahoo.com>
2024-06-07 20:20:47 +0330

    fix formik select awnings

--- src/components/formik/multiSelect/index.js
+++ src/components/formik/multiSelect/index.js
@@ -1,15 +1,12 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
+import { Checkbox } from '@mui/material';
 import Box from '@mui/material/Box';
-import OutlinedInput from '@mui/material/OutlinedInput';
+import Chip from '@mui/material/Chip';
+import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
 import Select from '@mui/material/Select';
-import Chip from '@mui/material/Chip';
 import { useField, useFormikContext } from 'formik';
-import { Checkbox } from '@mui/material';
-import { isObject } from 'lodash';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -22,15 +19,6 @@ const MenuProps = {
   }
 };
 
-function getStyles(name, personName, theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium
-  };
-}
-
 export default function MultipleSelect({
   name = '',
   options = [],
@@ -38,8 +26,6 @@ export default function MultipleSelect({
   labelName,
   labelName2
 }) {
-  const theme = useTheme();
-
   const [field, mata] = useField(name);
   const { setFieldValue } = useFormikContext();
   const handleChange = (event) => {
@@ -61,13 +47,13 @@ export default function MultipleSelect({
           labelId={name}
           id={name}
           multiple
-          value={field.value}
+          value={field.value || []}
           onChange={handleChange}
           input={<OutlinedInput id={name} label={label} />}
           renderValue={(selected) => (
             <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
               {selected.map((value) => (
-                <Chip key={value} label={options.find((i) => +i.id === +value).name} />
+                <Chip key={value} label={options.find((i) => +i.id === +value)?.name} />
               ))}
             </Box>
           )}
@@ -83,14 +69,12 @@ export default function MultipleSelect({
                 }}
                 key={pos}
                 value={options[item].id}
-                style={getStyles(field.value, field.value, theme)}
                 disableTouchRipple
               >
                 <Checkbox
                   disableRipple
                   checked={!!field.value.find((i) => +i === +options[item].id)}
                 />
-                {console.log(field.value.find((i) => +i === +options[item].id))}
                 {labelName && options[item][labelName]}
                 {labelName2 && labelName2(options[item])}
               </MenuItem>
