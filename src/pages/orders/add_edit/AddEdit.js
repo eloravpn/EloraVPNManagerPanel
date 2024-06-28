5668ddda2616e5c717bbfa50edfd9374e77418e3
ZCode75 <amir.sr75@yahoo.com>
2024-06-28 18:45:35 +0330

    fix acc

--- src/pages/orders/add_edit/AddEdit.js
+++ src/pages/orders/add_edit/AddEdit.js
@@ -319,7 +319,12 @@ const AddEdit = (props) => {
                   }}
                 />
               </Grid>
-
+              <Grid item xs={3}>
+                <TextField label={'DIS'} type="tel" name="dis" />
+              </Grid>
+              <Grid item xs={9}>
+                <ExtraField label={'Extera Discount'} price name="extra_discount" />
+              </Grid>
               {values.service_id ? (
                 <Grid item xs={12}>
                   <Box textAlign={'left'} m={1}>
@@ -390,12 +395,6 @@ const AddEdit = (props) => {
                   </Grid>
                 </>
               )}
-              <Grid item xs={3}>
-                <TextField label={'DIS'} type="tel" name="dis" />
-              </Grid>
-              <Grid item xs={9}>
-                <ExtraField label={'Extera Discount'} price name="extra_discount" />
-              </Grid>
             </Grid>
             <DialogActions>
               <Button
